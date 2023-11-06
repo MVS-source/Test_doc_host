3ff44a1cf9e726128b597b30cf8abe5de50377ba
MVS-source <victor-michael.smith@epitech.eu>
2023-11-06 22:08:15 +0100

    Update documentation

--- search/all_8.js
+++ search/all_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['m_5fheader_0',['m_header',['../structnetwork_1_1m__header.html',1,'network']]],
-  ['md_5fdocumentation_5fdevelopper_5fdocumentation_5ftutorials_5fand_5fhowtos_5ftutorial_1',['md_Documentation_Developper_Documentation_Tutorials_and_HowTos_tutorial',['../md__documentation__developper__documentation__tutorials_and__how_tos_tutorial.html',1,'']]],
+  ['md_5fdocumentation_5ftutorial_1',['md_Documentation_tutorial',['../md__documentation_tutorial.html',1,'']]],
   ['menu_2',['Menu',['../class_menu.html',1,'']]],
   ['message_3',['message',['../structnetwork_1_1message.html',1,'network']]],
   ['music_4',['Music',['../class_music.html',1,'']]]
