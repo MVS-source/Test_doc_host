8e80209327acefdfe07d5e48063abf562474a779
deli <victor-michael.smith@epitech.eu>
2023-11-06 22:14:48 +0100

    daw

--- search/all_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['background_0',['Background',['../class_background.html',1,'']]],
-  ['bullet_1',['Bullet',['../class_bullet.html',1,'']]],
-  ['button_2',['Button',['../class_button.html',1,'']]],
-  ['buttonbox_3',['ButtonBox',['../class_button_box.html',1,'']]]
-];
--- search/classes_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['soundmanager_0',['SoundManager',['../class_sound_manager.html',1,'']]]
-];
