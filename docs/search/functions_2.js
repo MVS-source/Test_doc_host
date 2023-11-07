4fd3c833e882a04d0c177bd22da0c83d155a187e
deli <victor-michael.smith@epitech.eu>
2023-11-07 03:42:37 +0100

    dwa

--- docs/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['getcstreambuffer_0',['getCStreamBuffer',['../class_c_stream_buffer.html#ad13ffb90ffbb1c46726cdb438e54ad9f',1,'CStreamBuffer']]]
-];
--- docs/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['io_5fservice_0',['io_service',['../class_client.html#a6fa60906067a5642c9f17aad521231b4',1,'Client']]]
-];
