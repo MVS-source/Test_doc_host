9eadcd8abd8d52d17cab24f46048b419bab2ff63
deli <victor-michael.smith@epitech.eu>
2023-11-07 01:56:42 +0100

    awd

--- docs/search/functions_5.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['readdata_0',['readData',['../class_c_stream_buffer.html#a051ab470ca96e88cedecb54d9eab14ef',1,'CStreamBuffer']]],
-  ['reciever_1',['reciever',['../class_client.html#ad5cbc18cee790c8f520e640d452235a4',1,'Client']]],
-  ['renderer_2',['Renderer',['../class_renderer.html#a7ebf46f54dab9905f79b80f7fddb76a6',1,'Renderer']]],
-  ['rtypeserver_3',['RTypeServer',['../class_r_type_server.html#a5aa93fbafa41c6a9dea9838a38ca5617',1,'RTypeServer']]]
-];
