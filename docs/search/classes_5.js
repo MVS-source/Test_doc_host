56936dae9ccbcbc600d8073ae28646cc209baa23
deli <victor-michael.smith@epitech.eu>
2023-11-07 01:45:40 +0100

    [ADD]

--- docs/search/classes_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['ibox_0',['IBox',['../class_i_box.html',1,'']]],
-  ['iclient_1',['IClient',['../classnetwork_1_1_i_client.html',1,'network']]]
-];
--- docs/search/classes_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['player_0',['Player',['../class_player.html',1,'']]],
-  ['promptbox_1',['PromptBox',['../class_prompt_box.html',1,'']]]
-];
