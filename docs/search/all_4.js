4f98a33c2ea423088bad9f898c78e78033a21912
deli <victor-michael.smith@epitech.eu>
2023-11-07 06:07:26 +0100

    daw

--- docs/search/all_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['game_0',['Game',['../class_game.html',1,'']]],
-  ['gameobject_1',['GameObject',['../class_game_object.html',1,'']]],
-  ['getcstreambuffer_2',['getCStreamBuffer',['../class_c_stream_buffer.html#ad13ffb90ffbb1c46726cdb438e54ad9f',1,'CStreamBuffer']]]
-];
--- docs/search/pages_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['r_2dtype_20_5bepitech_20project_5d_20_5bdocumentation_5d_0',['R-TYPE [EPITECH PROJECT] [DOCUMENTATION]',['../md__r_e_a_d_m_e.html',1,'']]]
-];
