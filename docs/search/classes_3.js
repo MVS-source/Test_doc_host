5a4d39b34031ef20bdaebc72ac756fe6a706d2dc
deli <victor-michael.smith@epitech.eu>
2023-11-07 03:10:55 +0100

    dwa

--- docs/search/classes_3.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['data_0',['Data',['../struct_data.html',1,'']]],
-  ['data_1',['data',['../structdata.html',1,'']]],
-  ['drawable_2',['Drawable',['../class_drawable.html',1,'']]],
-  ['drawableparams_3',['DrawableParams',['../struct_drawable_params.html',1,'']]]
-];
--- docs/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['md_5fdocumentation_5ftutorial_0',['md_Documentation_tutorial',['../md__documentation_tutorial.html',1,'']]]
-];
