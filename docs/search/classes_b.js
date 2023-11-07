0db447f49d8e049b3b0f973353c42503694bd5ce
deli <victor-michael.smith@epitech.eu>
2023-11-07 06:44:41 +0100

    d

--- docs/search/classes_b.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['unit_0',['Unit',['../class_unit.html',1,'']]],
-  ['updserver_1',['UPDServer',['../class_u_p_d_server.html',1,'']]]
-];
--- docs/search/functions_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['initclient_0',['initClient',['../class_client.html#ab5a8b80802c438dc16484efd9502eaf4',1,'Client']]],
-  ['initrenderer_1',['initRenderer',['../class_renderer.html#a40496a842d72dd1cfd9f0ecafbbe5a30',1,'Renderer']]]
-];
